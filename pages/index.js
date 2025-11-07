f96874aeb8b3a824ef183654ee2299f41c6b9539
vasbelco <vasbelco@gmail.com>
2025-11-07 07:14:18 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// ------- pages/index.js -------
+export default function Home() {
+return (
+<main style={{fontFamily:'sans-serif', padding: 24}}>
+<h1>Trasari MVP</h1>
+<p>Primer deploy ok. Luego añadimos /api/signup.</p>
+</main>
+)
+}
