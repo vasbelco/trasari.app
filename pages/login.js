0fbb3a158ceaf5521c5975c8e72c767d58dacc52
vasbelco <vasbelco@gmail.com>
2025-11-07 13:13:23 -0500

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,5 @@
+import LoginCard from '../components/LoginCard'
+
 export default function LoginPage() {
-  return <h1 style={{textAlign:'center', marginTop: '4rem'}}>Iniciar sesión</h1>;
+  return <LoginCard />
 }
